49f0599300cf9d6c04619d530ba52cd752f994e1
Bruno Pinho <brunopinho@outlook.com>
2024-03-08 20:11:36 +0000

    feat (arrays and slices chapter): create Sum and TestSum functions in arraysslices package

---
+++ arrays_slices/sum_test.go
@@ -0,0 +1,13 @@
+package arraysslices
+
+import "testing"
+
+func TestSum(t *testing.T) {
+	numbers := [5]int{1, 2, 3, 4, 5}
+	got := Sum(numbers)
+	want := 15
+
+	if got != want {
+		t.Errorf("expected %d but got %d, %v", want, got, numbers)
+	}
+}
