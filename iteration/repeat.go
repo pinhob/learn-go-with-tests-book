505a923d0efd72b533d0d5a36f00a659f7caac2d
Bruno Pinho <brunopinho@outlook.com>
2024-03-08 19:31:15 +0000

    feat (iteration chapter): refactor Repeat func, create BenchmarkRepeat and ExampleRepeat

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,10 +1,10 @@
 package iteration
 
-func Repeat(character string) string {
+func Repeat(character string, count int) string {
 	var repeated string
-  for i := 0; i < 5; i++ {
-    repeated += character
-  }
+	for i := 0; i < count; i++ {
+		repeated += character
+	}
 
-  return repeated
+	return repeated
 }
--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -1,12 +1,27 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	repeat := Repeat("a")
-	expected := "aaaaa"
+	repeat := Repeat("a", 3)
+	expected := "aaa"
 
 	if repeat != expected {
 		t.Errorf("expected %s but got %s", expected, repeat)
 	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
+	}
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("b", 3)
+	fmt.Println(repeated)
+	// Output: bbb
 }
