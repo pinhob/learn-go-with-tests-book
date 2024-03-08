459f9cdef4cc578b1649653aa99b64fee923eeb5
pinhob <32676559-pinhob@users.noreply.replit.com>
2024-03-08 18:48:56 +0000

    feat (hello, world chapter): write more tests, add language func argument and improve code quality

--- hello_test.go
+++ hello_test.go
@@ -3,9 +3,44 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	result := Hello("Replit")
-	expected := "Hello, Replit!"
+	t.Run("saying hello to people", func(t *testing.T) {
+		result := Hello("Replit", "")
+		expected := "Hello, Replit"
 
+		assertCorrectmessage(t, result, expected)
+	})
+
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		result := Hello("", "")
+		expected := "Hello, World"
+
+		assertCorrectmessage(t, result, expected)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		result := Hello("Nachos", "Spanish")
+		expected := "Hola, Nachos"
+
+		assertCorrectmessage(t, result, expected)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		result := Hello("Poirot", "French")
+		expected := "Bonjour, Poirot"
+
+		assertCorrectmessage(t, result, expected)
+	})
+
+	t.Run("in Portuguese", func(t *testing.T) {
+		result := Hello("Calabreso", "Portuguese")
+		expected := "Oi, Calabreso"
+
+		assertCorrectmessage(t, result, expected)
+	})
+}
+
+func assertCorrectmessage(t testing.TB, result, expected string) {
+	t.Helper()
 	if result != expected {
 		t.Errorf("result: '%s', expected: '%s'", result, expected)
 	}
--- main.go
+++ main.go
@@ -4,10 +4,40 @@ import (
 	"fmt"
 )
 
-func Hello(name string) string {
-  return "Hello, " + name + "!"
+const (
+	spanish    = "Spanish"
+	french     = "French"
+	portuguese = "Portuguese"
+
+	englishHelloPrefix    = "Hello, "
+	spanishHelloPrefix    = "Hola, "
+	frenchHelloPrefix     = "Bonjour, "
+	portugueseHelloPrefix = "Oi, "
+)
+
+func Hello(name, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	case portuguese:
+		prefix = portugueseHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	return
 }
 
 func main() {
-	fmt.Println(Hello("Replit"))
+	fmt.Println(Hello("Replit", ""))
 }
