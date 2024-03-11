ab4c1e7ee94567e4dc95b5abef1c206e9ca3040f
Bruno Pinho <brunopinho@outlook.com>
2024-03-11 17:50:09 +0000

    feat (structs chapter): refactor code

--- structs_methods_interfaces/shapes_test.go
+++ structs_methods_interfaces/shapes_test.go
@@ -13,20 +13,42 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	checkArea := func(t testing.TB, shape Shape, want float64) {
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{shape: Rectangle{Width: 10.0, Height: 10.0}, hasArea: 100.0},
+		{shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
 	}
 
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{10.0, 10.0}
-		checkArea(t, rectangle, 100.0)
-	})
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+			}
+		})
+	}
 
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		checkArea(t, circle, 314.1592653589793)
-	})
+	/*
+		  or this way, trying inidivdually with a helper function:
+			checkArea := func(t testing.TB, shape Shape, want float64) {
+				got := shape.Area()
+				if got != want {
+		      t.Errorf("got %g want %g", got, want)
+				}
+			}
+
+			t.Run("rectangles", func(t *testing.T) {
+				rectangle := Rectangle{10.0, 10.0}
+				checkArea(t, rectangle, 100.0)
+			})
+
+			t.Run("circles", func(t *testing.T) {
+				circle := Circle{10}
+				checkArea(t, circle, 314.1592653589793)
+			})
+	*/
 }
