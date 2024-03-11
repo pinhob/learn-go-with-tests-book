760d3883ef53d4facaecfb08e5137c451c97f3f4
Bruno Pinho <brunopinho@outlook.com>
2024-03-11 18:02:34 +0000

    feat(pointers chapter): create TestWallet and Wallet functions

---
+++ pointers_errors/wallet.go
@@ -0,0 +1,25 @@
+package pe
+
+import "fmt"
+
+type Bitcoin int
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+type Stringer interface {
+	String() string
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	w.balance += amount
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
