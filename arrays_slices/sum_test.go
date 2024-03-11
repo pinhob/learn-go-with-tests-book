e52fd22586a7c4361fb8a362ef9ccea6fde9f639
Bruno Pinho <brunopinho@outlook.com>
2024-03-11 14:08:51 +0000

    feat (arrays and slices chapter): refactor sum to slices

--- arrays_slices/sum_test.go
+++ arrays_slices/sum_test.go
@@ -3,11 +3,24 @@ package arraysslices
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
-	got := Sum(numbers)
-	want := 15
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
+		got := Sum(numbers)
+		want := 15
 
-	if got != want {
-		t.Errorf("expected %d but got %d, %v", want, got, numbers)
-	}
+		if got != want {
+			t.Errorf("expected %d but got %d, %v", want, got, numbers)
+		}
+	})
+
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		want := 6
+
+		if got != want {
+			t.Errorf("got %d wnat %d given, %v", got, want, numbers)
+		}
+	})
 }
