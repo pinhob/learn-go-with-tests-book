f698038d6fd519d99e2b27b0862a93b06fe5bcf6
Bruno Pinho <brunopinho@outlook.com>
2024-03-11 18:36:36 +0000

    feat (pointers chapter): create TestWithdraw and Withdraw functions and check for errors

--- pointers_errors/wallet.go
+++ pointers_errors/wallet.go
@@ -1,6 +1,11 @@
 package pe
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
+
+var ErrInsufficientFunds = errors.New("cannot withdraw, insufficent funds")
 
 type Bitcoin int
 
@@ -22,4 +27,13 @@ func (w *Wallet) Deposit(amount Bitcoin) {
 
 func (w *Wallet) Balance() Bitcoin {
 	return w.balance
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return ErrInsufficientFunds
+	}
+
+	w.balance -= amount
+	return nil
 }
