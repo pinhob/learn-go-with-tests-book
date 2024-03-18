b93d65e388a0d604ead14e64df4d30a2d31b38bf
Bruno Pinho <brunopinho@outlook.com>
2024-03-18 18:53:50 +0000

    feat (context chapter): create spy helper functions

--- context/context_test.go
+++ context/context_test.go
@@ -11,6 +11,7 @@ import (
 type SpyStore struct {
 	response  string
 	cancelled bool
+	t         *testing.T
 }
 
 func (s *SpyStore) Fetch() string {
@@ -22,24 +23,25 @@ func (s *SpyStore) Cancel() {
 	s.cancelled = true
 }
 
+func (s *SpyStore) assertWasCancelled() {
+	s.t.Helper()
+	if !s.cancelled {
+		s.t.Error("store was not told to cancel")
+	}
+}
+
+func (s *SpyStore) assertWasNotCancelled() {
+	s.t.Helper()
+	if s.cancelled {
+		s.t.Error("Store was told to cancel")
+	}
+}
+
 func TestServer(t *testing.T) {
-	t.Run("receive 'hello, world' from the server", func(t *testing.T) {
-		data := "hello, world"
-		svr := Server(&SpyStore{response: data})
-
-		request := httptest.NewRequest(http.MethodGet, "/", nil)
-		response := httptest.NewRecorder()
-
-		svr.ServeHTTP(response, request)
-
-		if response.Body.String() != data {
-			t.Errorf(`got %s, want %s`, response.Body.String(), data)
-		}
-	})
+	data := "hello, world"
 
 	t.Run("tells store to cancel work if request is cancelled", func(t *testing.T) {
-		data := "hello, world"
-		store := &SpyStore{response: data}
+		store := &SpyStore{response: data, t: t}
 		svr := Server(store)
 
 		request := httptest.NewRequest(http.MethodGet, "/", nil)
@@ -52,14 +54,11 @@ func TestServer(t *testing.T) {
 
 		svr.ServeHTTP(response, request)
 
-		if !store.cancelled {
-			t.Error("store was not told to cancel")
-		}
+		store.assertWasCancelled()
 	})
 
 	t.Run("returns data from store", func(t *testing.T) {
-		data := "hello, world"
-		store := &SpyStore{response: data}
+		store := &SpyStore{response: data, t: t}
 		svr := Server(store)
 
 		request := httptest.NewRequest(http.MethodGet, "/", nil)
@@ -71,8 +70,6 @@ func TestServer(t *testing.T) {
 			t.Errorf(`got %s, want %s`, response.Body.String(), data)
 		}
 
-		if store.cancelled {
-			t.Error("it should not have cancelled the store")
-		}
+		store.assertWasNotCancelled()
 	})
 }
