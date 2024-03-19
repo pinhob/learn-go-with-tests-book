45af418f372c192e6ac0865eb9c58699cfc3199f
Bruno Pinho <brunopinho@outlook.com>
2024-03-19 15:30:49 +0000

    feat (roman numerals chapter): create numerals_test and roman_numerals files and basic functions

---
+++ roman_numerals/numerals_test.go
@@ -0,0 +1,12 @@
+package roman
+
+import "testing"
+
+func TestRomanNumerals(t *testing.T) {
+	got := ConvertToRoman(1)
+	want := "I"
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
