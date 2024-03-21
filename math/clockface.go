7bbd6f4aabfbcc9af4b08db0517ca8dca45d645a
Bruno Pinho <brunopinho@outlook.com>
2024-03-21 19:11:58 +0000

    feeat (math chapter): create SecondHandPoint

--- math/clockface.go
+++ math/clockface.go
@@ -14,6 +14,14 @@ func SecondHand(t time.Time) Point {
 	return Point{150, 60}
 }
 
+func SecondHandPoint(t time.Time) Point {
+	angle := secondsInRadians(t)
+	x := math.Sin(angle)
+	y := math.Cos(angle)
+
+	return Point{x, y}
+}
+
 func secondsInRadians(t time.Time) float64 {
 	return (math.Pi / (30 / float64(t.Second())))
 }
