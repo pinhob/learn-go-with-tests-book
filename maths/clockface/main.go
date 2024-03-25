2d31fc9d5e05f225a3f51b46019a0876ab671672
pinhob <20286747+pinhob@users.noreply.github.com>
2024-03-25 20:20:47 -0300

    fix (maths chapter): import clockface module and create clock svg

--- maths/clockface/main.go
+++ maths/clockface/main.go
@@ -1,10 +1,12 @@
-package clockface
+package main
 
 import (
 	"fmt"
 	"io"
 	"os"
 	"time"
+
+	"clockface"
 )
 
 func main() {
@@ -23,10 +25,10 @@ func secondHandTag(p clockface.Point) string {
 const svgStart = `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
 <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
 <svg xmlns="http://www.w3.org/2000/svg"
-     width="100%"
-     height="100%"
-     viewBox="0 0 300 300"
-     version="2.0">`
+			width="100%"
+			height="100%"
+			viewBox="0 0 300 300"
+			version="2.0">`
 
 const bezel = `<circle cx="150" cy="150" r="100" style="fill:#fff;stroke:#000;stroke-width:5px;"/>`
 
