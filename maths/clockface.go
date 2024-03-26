98bcc9f0dfc0570da4d14f9af3af4ef8c5e993cf
pinhob <20286747+pinhob@users.noreply.github.com>
2024-03-25 22:18:42 -0300

    feat (maths chapter): refactor clockface and main.go and create TestSVGWriterAtMidnight

--- maths/clockface.go
+++ maths/clockface.go
@@ -1,6 +1,8 @@
 package clockface
 
 import (
+	"fmt"
+	"io"
 	"math"
 	"time"
 )
@@ -10,16 +12,26 @@ type Point struct {
 	Y float64
 }
 
-const secondHandLength = 90
-const clockCentreX = 150
-const clockCentryY = 150
+const (
+	secondHandLength = 90
+	clockCentreX     = 150
+	clockCentryY     = 150
+)
 
-func SecondHand(t time.Time) Point {
+func SVGWriter(w io.Writer, t time.Time) {
+	io.WriteString(w, svgStart)
+	io.WriteString(w, bezel)
+	SecondHand(w, t)
+	io.WriteString(w, svgEnd)
+
+}
+
+func SecondHand(w io.Writer, t time.Time) {
 	p := SecondHandPoint(t)
 	p = Point{p.X * secondHandLength, p.Y * secondHandLength} // scale
 	p = Point{p.X, -p.Y}                                      // flip
 	p = Point{p.X + clockCentreX, p.Y + clockCentryY}         // translate
-	return p
+	fmt.Fprintf(w, `<line x1="150" y1="150" x2="%.3f" y2="%.3f" style="fill:none;stroke:#f00;stroke-width:3px;"/>`, p.X, p.Y)
 }
 
 func SecondHandPoint(t time.Time) Point {
@@ -33,3 +45,15 @@ func SecondHandPoint(t time.Time) Point {
 func secondsInRadians(t time.Time) float64 {
 	return (math.Pi / (30 / float64(t.Second())))
 }
+
+const svgStart = `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg xmlns="http://www.w3.org/2000/svg"
+			width="100%"
+			height="100%"
+			viewBox="0 0 300 300"
+			version="2.0">`
+
+const bezel = `<circle cx="150" cy="150" r="100" style="fill:#fff;stroke:#000;stroke-width:5px;"/>`
+
+const svgEnd = `</svg>`
