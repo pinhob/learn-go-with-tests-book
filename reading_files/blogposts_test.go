f078ccea5339d89bf34bf002427dc0c4e1b78646
pinhob <20286747+pinhob@users.noreply.github.com>
2024-04-01 19:18:35 -0300

    feat (reading files chapter): add err as return

--- reading_files/blogposts_test.go
+++ reading_files/blogposts_test.go
@@ -1,17 +1,31 @@
 package blogposts
 
 import (
+	"errors"
+	"io/fs"
 	"testing"
 	"testing/fstest"
 )
 
+type StubFailingFS struct {
+}
+
+func (s StubFailingFS) Open(name string) (fs.File, error) {
+	return nil, errors.New("oh no, i alaways fail")
+}
+
 func TestNewBlogPosts(t *testing.T) {
 	fs := fstest.MapFS{
-		"hello-world.md":  {Data: []byte("hi")},
+		"hello-world.md":  {Data: []byte("i")},
 		"hello-world2.md": {Data: []byte("hola")},
 	}
 
-	posts := NewPostsFromFS(fs)
+	posts, err := NewPostsFromFS(fs)
+	// posts, err := NewPostsFromFS(StubFailingFS{})
+
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	if len(posts) != len(fs) {
 		t.Errorf("got %d posts, wanted %d posts", len(posts), len(fs))
