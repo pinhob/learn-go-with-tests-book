b6a5aa3b616b4dbee547996af19bf6c531b9fc1f
pinhob <20286747+pinhob@users.noreply.github.com>
2024-04-03 18:11:07 -0300

    feat (html templates chapter): add RenderIndex and test

--- html_templates/blogrenderer.go
+++ html_templates/blogrenderer.go
@@ -4,6 +4,7 @@ import (
 	"embed"
 	"html/template"
 	"io"
+	"strings"
 )
 
 type Post struct {
@@ -11,18 +12,45 @@ type Post struct {
 	Tags                     []string
 }
 
+type PostRenderer struct {
+	templ *template.Template
+}
+
 var (
 	//go:embed "templates/*"
 	postTemplates embed.FS
 )
 
-func Render(w io.Writer, p Post) error {
+func NewPostRender() (*PostRenderer, error) {
 	templ, err := template.ParseFS(postTemplates, "templates/*.gohtml")
+	if err != nil {
+		return nil, err
+	}
+
+	return &PostRenderer{templ: templ}, nil
+}
+
+func (r *PostRenderer) RenderIndex(w io.Writer, posts []Post) error {
+	indexTemplate := `<ol>{{range .}}<li><a href="/post/{{sanitiseTitle .Title}}">{{.Title}}</a></li>{{end}}</ol>`
+
+	templ, err := template.New("index").Funcs(template.FuncMap{
+		"sanitiseTitle": func(title string) string {
+			return strings.ToLower(strings.Replace(title, " ", "-", -1))
+		},
+	}).Parse(indexTemplate)
 	if err != nil {
 		return err
 	}
 
-	if err := templ.ExecuteTemplate(w, "blog.gohtml", p); err != nil {
+	if err := templ.Execute(w, posts); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *PostRenderer) Render(w io.Writer, p Post) error {
+	if err := r.templ.ExecuteTemplate(w, "blog.gohtml", p); err != nil {
 		return err
 	}
 
