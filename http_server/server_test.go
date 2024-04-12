f696ffe52508d55c6f421864fdfb2e3f878539ba
pinhob <20286747+pinhob@users.noreply.github.com>
2024-04-12 19:33:23 -0300

    feat (http server chapter): test and implementnot found status

--- http_server/server_test.go
+++ http_server/server_test.go
@@ -30,6 +30,7 @@ func TestGETPlayers(t *testing.T) {
 
 		server.ServeHTTP(response, request)
 
+		assertStatus(t, response.Code, http.StatusOK)
 		assertResponseBody(t, response.Body.String(), "20")
 	})
 
@@ -39,7 +40,20 @@ func TestGETPlayers(t *testing.T) {
 
 		server.ServeHTTP(response, request)
 
+		assertStatus(t, response.Code, http.StatusOK)
 		assertResponseBody(t, response.Body.String(), "10")
+	})
+
+	t.Run("returns 404 on missing players", func(t *testing.T) {
+		request := newGetScoreRequest("Apollo")
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		got := response.Code
+		want := http.StatusNotFound
+
+		assertStatus(t, got, want)
 	})
 }
 
@@ -53,4 +67,12 @@ func assertResponseBody(t testing.TB, got, want string) {
 	if got != want {
 		t.Errorf("response body is wrong, got %q, want %q", got, want)
 	}
+}
+
+func assertStatus(t testing.TB, got, want int) {
+
+	if got != want {
+		t.Errorf("incorrect status, got status %d want %d", got, want)
+	}
+
 }
