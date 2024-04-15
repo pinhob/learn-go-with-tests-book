901bcec6362a75b15bbe8af3fd1118a4348eb03c
pinhob <20286747+pinhob@users.noreply.github.com>
2024-04-15 17:27:56 -0300

    refactor (io chapter): create temp files

--- http_server/file_system_store_test.go
+++ http_server/file_system_store_test.go
@@ -1,16 +1,19 @@
 package main
 
 import (
-	"strings"
+	"io"
+	"os"
 	"testing"
 )
 
 func TestFileSystemStore(t *testing.T) {
 	t.Run("league from a render", func(t *testing.T) {
-		database := strings.NewReader(`[
+		database, cleanDatabase := createTempFile(t, `[
 			{ "Name": "Cleo", "Wins": 10 },
 			{ "Name": "Chris", "Wins": 33 }
 		]`)
+		defer cleanDatabase()
+
 		store := FileSystemPlayerStore{database}
 
 		got := store.GetLeague()
@@ -24,10 +27,12 @@ func TestFileSystemStore(t *testing.T) {
 	})
 
 	t.Run("get player score", func(t *testing.T) {
-		database := strings.NewReader(`[
+		database, cleanDatabase := createTempFile(t, `[
 			{ "Name": "Cleo", "Wins": 10 },
 			{ "Name": "Chris", "Wins": 33 }
 		]`)
+		defer cleanDatabase()
+
 		store := FileSystemPlayerStore{database}
 
 		got := store.GetPlayerScore("Chris")
@@ -44,4 +49,23 @@ func assertScoreEqulas(t testing.TB, got, want int) {
 	if got != want {
 		t.Errorf("got %d want %d", got, want)
 	}
+}
+
+func createTempFile(t testing.TB, initialData string) (io.ReadWriteSeeker, func()) {
+	t.Helper()
+
+	tmpfile, err := os.CreateTemp("", "db")
+
+	if err != nil {
+		t.Fatalf("could not create temp file %v", err)
+	}
+
+	tmpfile.Write([]byte(initialData))
+
+	removeFile := func() {
+		tmpfile.Close()
+		os.Remove(tmpfile.Name())
+	}
+
+	return tmpfile, removeFile
 }
