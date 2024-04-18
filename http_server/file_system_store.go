4e0c204b5c8eec9c4c48ee203af832b04366c2b2
pinhob <20286747+pinhob@users.noreply.github.com>
2024-04-18 15:43:20 -0300

    refactor (io chapter): change tape file type to *os.FIle

--- http_server/file_system_store.go
+++ http_server/file_system_store.go
@@ -3,18 +3,19 @@ package main
 import (
 	"encoding/json"
 	"io"
+	"os"
 )
 
 type FileSystemPlayerStore struct {
-	database io.ReadWriteSeeker
+	database io.Writer
 	league   League
 }
 
-func NewFileSystemPLayerStore(database io.ReadWriteSeeker) *FileSystemPlayerStore {
+func NewFileSystemPLayerStore(database *os.File) *FileSystemPlayerStore {
 	database.Seek(0, 0)
 	league, _ := NewLeague(database)
 	return &FileSystemPlayerStore{
-		database: database,
+		database: &tape{database},
 		league:   league,
 	}
 }
@@ -42,6 +43,5 @@ func (f *FileSystemPlayerStore) RecordWin(name string) {
 		f.league = append(f.league, Player{name, 1})
 	}
 
-	f.database.Seek(0, 0)
 	json.NewEncoder(f.database).Encode(f.league)
 }
