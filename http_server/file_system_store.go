76d89f4afdd2933eed0e8f07d1a5c1e95f4966d3
pinhob <20286747+pinhob@users.noreply.github.com>
2024-04-18 16:04:33 -0300

    refactor (io chapter): create initialisePlayerDBFile func

--- http_server/file_system_store.go
+++ http_server/file_system_store.go
@@ -11,28 +11,39 @@ type FileSystemPlayerStore struct {
 	league   League
 }
 
-func NewFileSystemPLayerStore(database *os.File) (*FileSystemPlayerStore, error) {
-	database.Seek(0, 0)
+func initialisePlayerDBFile(file *os.File) error {
 
-	info, err := database.Stat()
+	file.Seek(0, 0)
+
+	info, err := file.Stat()
 
 	if err != nil {
-		return nil, fmt.Errorf("problem getting file info from file %s, %v", database.Name(), err)
+		return fmt.Errorf("problem getting file info from file %s, %v", file.Name(), err)
 	}
 
 	if info.Size() == 0 {
-		database.Write([]byte("[]"))
-		database.Seek(0, 0)
+		file.Write([]byte("[]"))
+		file.Seek(0, 0)
 	}
 
-	league, err := NewLeague(database)
+	return nil
+}
+
+func NewFileSystemPLayerStore(file *os.File) (*FileSystemPlayerStore, error) {
+	err := initialisePlayerDBFile(file)
 
 	if err != nil {
-		return nil, fmt.Errorf("problem loading player store from file %s, %v", database.Name(), err)
+		return nil, fmt.Errorf("problem initialising player db  file, %v", err)
+	}
+
+	league, err := NewLeague(file)
+
+	if err != nil {
+		return nil, fmt.Errorf("problem loading player store from file %s, %v", file.Name(), err)
 	}
 
 	return &FileSystemPlayerStore{
-		database: json.NewEncoder(&tape{database}),
+		database: json.NewEncoder(&tape{file}),
 		league:   league,
 	}, nil
 }
