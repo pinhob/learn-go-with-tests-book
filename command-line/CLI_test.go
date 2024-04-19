cf823194e04fdc4426ead71f8a63c4647e91f11f
pinhob <20286747+pinhob@users.noreply.github.com>
2024-04-19 13:05:40 -0300

    feat (time chapter): create basic alerter and test

--- command-line/CLI_test.go
+++ command-line/CLI_test.go
@@ -4,14 +4,44 @@ import (
 	poker "main/command-line"
 	"strings"
 	"testing"
+	"time"
 )
 
+type SpyBlindAlerter struct {
+	alerts []struct {
+		scheduletAt time.Duration
+		amount      int
+	}
+}
+
+func (s *SpyBlindAlerter) ScheduleAlertAt(duration time.Duration, amount int) {
+	s.alerts = append(s.alerts, struct {
+		scheduletAt time.Duration
+		amount      int
+	}{duration, amount})
+}
+
+var dummySpyAlerter = &SpyBlindAlerter{}
+
 func TestCLI(t *testing.T) {
+	t.Run("it schedules printing of blind values", func(t *testing.T) {
+		in := strings.NewReader("Chris wins\n")
+		playerStore := &poker.StubPlayerStore{}
+		blindAlerter := &SpyBlindAlerter{}
+
+		cli := poker.NewCLI(playerStore, in, blindAlerter)
+		cli.PlayPoker()
+
+		if len(blindAlerter.alerts) != 1 {
+			t.Fatal("expected a blind alert to be scheduled")
+		}
+	})
+
 	t.Run("record Chris win from user input", func(t *testing.T) {
 		in := strings.NewReader("Chris wins\n")
 		playerStore := &poker.StubPlayerStore{}
 
-		cli := poker.NewCLI(playerStore, in)
+		cli := poker.NewCLI(playerStore, in, dummySpyAlerter)
 		cli.PlayPoker()
 
 		poker.AssertPlayerWin(t, playerStore, "Chris")
@@ -21,7 +51,7 @@ func TestCLI(t *testing.T) {
 		in := strings.NewReader("Cleo wins\n")
 		playerStore := &poker.StubPlayerStore{}
 
-		cli := poker.NewCLI(playerStore, in)
+		cli := poker.NewCLI(playerStore, in, dummySpyAlerter)
 		cli.PlayPoker()
 
 		poker.AssertPlayerWin(t, playerStore, "Cleo")
